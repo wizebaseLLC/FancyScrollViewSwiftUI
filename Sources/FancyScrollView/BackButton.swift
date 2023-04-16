4bb764f054a33fd549b9b1301ac72b6378f5e4ee
Wizebase <wizebase@gmail.com>
2023-04-15 20:35:26 -0400

    fixed back button hit radius

--- Sources/FancyScrollView/BackButton.swift
+++ Sources/FancyScrollView/BackButton.swift
@@ -4,13 +4,20 @@ struct BackButton: View {
     let onClose: () -> Void
     var body: some View {
             Button(action: onClose) {
-               Image(systemName: "chevron.left")
-                   .resizable()
-                   .aspectRatio(contentMode: .fit)
-                   .frame(height: 20, alignment: .leading)
-                   .foregroundColor(.accentColor)
-                   .padding(.horizontal, 16)
-                   .font(Font.body.bold())
+                ZStack(alignment: .leading) {
+                   Image(systemName: "chevron.left")
+                       .resizable()
+                       .aspectRatio(contentMode: .fit)
+                       .frame(height: 20, alignment: .leading)
+                       .foregroundColor(.accentColor)
+                       .font(Font.body.bold())
+                   
+                   Rectangle()
+                       .frame(width: 75, height: 75)
+                       .opacity(0.001)
+               }
+                
+                .padding(.horizontal, 16)
             }
     }
 }
